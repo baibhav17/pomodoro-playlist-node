85627fcfc2dc43e28f84046c754ec5a50dc2cf84
baibhav-celestial <baibhav.s@celestialsys.com>
2024-01-17 11:21:08 +0530

    initial commit- add and get user

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3020;
+
+app.use(cors());
+app.use(express.json());
+
+const defaultUsers = [
+    {
+      index: 1,
+      name: 'Baibhav Saxena',
+      userID: 'BS1',
+      password: 'Baibhav@123',
+    },
+    {
+      index: 2,
+      name: 'SS',
+      userID: 'SS1',
+      password: 'Baibhav@123',
+    },
+  ];
+
+
+  // create new user.
+  app.post('/createUser',(req,res)=>{
+      const {name,userID, password} = req.body;
+      const newUser = {index: defaultUsers.length + 1, name, userID, password};
+      defaultUsers.push(newUser);
+      res.status(200).json(newUser);
+  })
+
+  // Get the list of users.
+  app.get('/getUsers',(req,res)=>{
+      res.status(200).json(defaultUsers);
+  })
+
+ // Start the server
+ app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
